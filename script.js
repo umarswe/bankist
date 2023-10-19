3691a0e9d0d7520cb824385a30f148c44338e23d
umaar-kr <umaar.kr@gmail.com>
2023-10-19 23:22:04 +0900

    logging out account

--- script.js
+++ script.js
@@ -163,3 +163,16 @@ btnTransfer.addEventListener('click', function(e) {
       updateUI(currentAccount);
     }
 })
+
+btnClose.addEventListener('click', function(e){
+  e.preventDefault();
+
+  if(currentAccount.username === inputCloseUsername.value && currentAccount.pin === Number(inputClosePin.value)) {
+    const index = accounts.findIndex(acc => acc.username === currentAccount.username);
+    accounts.splice(index, 1);
+    containerApp.style.opacity = 0;
+  }
+
+  inputCloseUsername.value = inputClosePin = '';
+
+})
